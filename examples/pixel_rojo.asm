2bfd9cd477dcf5062e5c5839691d226bde6a7b32
ronaldo <ronaldo@cheesetea.com>
2016-05-02 02:23:43 +0200

    Fixed some parts of the example

--- examples/pixel_rojo.asm
+++ examples/pixel_rojo.asm
@@ -53,7 +53,7 @@ ret               ;; Devolver el control a quien nos haya llamado
 ;;    * Crea un nuevo fichero con File/New y copia el código de este ejemplo
 ;;       - (Alternativamente, abre este fichero con File/Open)
 ;;    * Pulsa F9 para ensamblar el código en la dirección &4000 de memoria
-;;    * Cierra el editor y teclea "CALL &4000" en el Amstrad CPC para ejecutar
+;;    * Cierra el editor y teclea CALL &4000 en el Amstrad CPC para ejecutar
 ;;
 ;; EXPLICACIÓN
 ;;
@@ -63,21 +63,21 @@ ret               ;; Devolver el control a quien nos haya llamado
 ;; se almacena en memoria como 2 bits. 
 ;;
 ;;    La zona de memoria donde se almacenan los píxeles es la que va de la 
-;; dirección &C000 en hexadecimal (48000 en decimal) hasta &FFFF (65535). Cada 
+;; dirección &C000 en hexadecimal (49152 en decimal) hasta &FFFF (65535). Cada 
 ;; posición de memoria tiene 8 bits (1 byte) y almacena los colores para 4 píxeles.
 ;; Los 8 bits tienen este aspecto:
 ;;
-;;    8 bits, 1 Byte: [ A B C D a b c d ]   Píxel 0: %Aa   Pixel 1: %Bb
+;;    8 bits, 1 Byte: [ a b c d A B C D ]   Píxel 0: %Aa   Pixel 1: %Bb
 ;;                                          Píxel 2: %Cc   Píxel 3: %Dd
 ;;
 ;;    Un ejemplo para entender el diagrama de arriba:
 ;;
 ;;       La posición &C000 de memoria contiene el byte [01011011]. Así pues,
 ;;       los valores de los 4 primeros píxeles son
-;;          * Píxel 0 = [0___1___] = %01 en binario, 1 en decimal (Color 1)
-;;          * Píxel 1 = [_1___0__] = %10 en binario, 2 en decimal (Color 2)
-;;          * Píxel 2 = [__0___1_] = %01 en binario, 1 en decimal (Color 1)
-;;          * Píxel 3 = [___1___1] = %11 en binario, 3 en decimal (Color 3)
-;;       Por tanto, los primeros 4 píxeles de pantalla tienen colores 1, 2, 1 y 3
+;;          * Píxel 0 = [0___1___] = %10 en binario (Color 2)
+;;          * Píxel 1 = [_1___0__] = %01 en binario (Color 1)
+;;          * Píxel 2 = [__0___1_] = %10 en binario (Color 2)
+;;          * Píxel 3 = [___1___1] = %11 en binario (Color 3)
+;;       Por tanto, los primeros 4 píxeles de pantalla tienen colores 2, 1, 2 y 3
 ;;       
 ;;===================================================================================
