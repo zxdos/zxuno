d5de44e85e864eb4656572a60d69b80f86b26437
antoniovillena <espineter@yahoo.com>
2016-05-04 22:25:41 +0200

    Actualizo Oric

---
+++ cores/Oric/build/make.bat
@@ -0,0 +1,7 @@
+SET machine=oric
+SET ruta_ucf=..\source\oric
+SET ruta_bat=..\..\
+call %ruta_bat%genxst.bat
+call %ruta_bat%generar.bat v2_v3
+call %ruta_bat%generar.bat v4
+call %ruta_bat%generar.bat Ap
