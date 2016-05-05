33a38487e189e093d0429fcdcda8dcbcade8462a
antoniovillena <espineter@yahoo.com>
2016-05-05 23:11:30 +0200

    Añado atari 2600

---
+++ cores/Atari2600/zxuno/make.bat
@@ -0,0 +1,8 @@
+SET machine=zxuno_a2601
+SET ruta_ucf=a2601
+SET ruta_bat=..\..\
+call %ruta_bat%genxst.bat
+call %ruta_bat%generar.bat v2
+call %ruta_bat%generar.bat v3
+call %ruta_bat%generar.bat v4
+call %ruta_bat%generar.bat Ap
