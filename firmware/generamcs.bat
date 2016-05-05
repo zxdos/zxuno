310afc4cd80b80899b40fbe8231ce4e2de084ab0
antoniovillena <espineter@yahoo.com>
2016-05-05 10:51:49 +0200

    Actualizo

--- firmware/generamcs.bat
+++ firmware/generamcs.bat
@@ -11,12 +11,12 @@ GenRom 0 203 0 0 0 Machine tmp.bin core_taps\SPECTRUM.TAP
 rem CgLeches core_taps\SPECTRUM.TAP core_wavs\SPECTRUM.WAV 4
 call :CreateMachine CORE2 "Sam Coupe"        SamCoupe\tld_sam.%2.bit 0 %3
 call :CreateMachine CORE3 "Jupiter ACE"      JupiterAce\jupiter_ace.%2.bit JupiterAce\jupiter_ace.v2_v3.bit %3
-rem call :CreateMachine CORE4 "Master System"    sms_v2_spartan6\test4\sms_final_v4.bit
-rem call :CreateMachine CORE5 "Oric Atmos"       oric_spartan6\test1\build\oric_v4.bit
-rem call :CreateMachine CORE6 "BBC Micro"        BBCMicro\test3\working\bbc_micro_v4.bit
-rem call :CreateMachine CORE7 "Apple ][ (VGA)"   Apple2_spartan6\test2\build\apple2_top_v4.bit
-rem call :CreateMachine CORE8 "Acorn Atom (VGA)" acorn_atom_spartan6\test2\working\atomic_top_zxuno_v4.bit
-rem call :CreateMachine CORE9 "NES (VGA)"        nes_v2_spartan6\test1_v4\xilinx\nes_zxuno_v4.bit
+call :CreateMachine CORE4 "Master System"    MasterSystem\sms_rgb.%2.bit 0 %3
+call :CreateMachine CORE5 "Oric Atmos"       Oric\build\oric.%3.bit Oric\build\oric.v2_v3.bit %3
+call :CreateMachine CORE6 "BBC Micro"        BBCMicro\working\bbc_micro.%2.bit 0 %3
+call :CreateMachine CORE7 "Apple 2 (VGA)"    Apple2\build\Apple2.%2.bit 0 %3
+call :CreateMachine CORE8 "Acorn Atom (VGA)" AcornAtom\AcornAtom.%2.bit 0 %3
+call :CreateMachine CORE9 "NES (VGA)"        NES\xilinx\nes_zxuno.%2.bit 0 %3
 copy /y rom_binaries\esxdos.rom sd_binaries\ESXDOS.%3
 copy /y firmware.rom sd_binaries\FIRMWARE.%3
 GenRom 0 202 0 0 0 BIOS firmware.rom core_taps\FIRMWARE.TAP
