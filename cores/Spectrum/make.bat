047534c23ed8f9fb07402461e7f9b2c788a69642
antoniovillena <espineter@yahoo.com>
2016-05-23 13:08:24 +0200

    Pongo generamcs_multi.bat para evitar confusión

--- cores/Spectrum/make.bat
+++ cores/Spectrum/make.bat
@@ -2,7 +2,7 @@ SET machine=tld_zxuno
 SET ruta_ucf=pines
 SET ruta_bat=..\
 call %ruta_bat%genxst.bat
-call %ruta_bat%generar.bat v2
-call %ruta_bat%generar.bat v3
+rem call %ruta_bat%generar.bat v2
+rem call %ruta_bat%generar.bat v3
 call %ruta_bat%generar.bat v4
-call %ruta_bat%generar.bat Ap
+rem call %ruta_bat%generar.bat Ap
--- firmware/generamcs_vx.bat
+++
@@ -1,4 +0,0 @@
-call generamcs 1 Ap ZZA
-call generamcs 2 v2 ZZ2
-call generamcs 3 v3 ZZ3
-call generamcs 4 v4 ZX1
