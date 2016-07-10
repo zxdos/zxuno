3985a724f7d17258187d0a0402d1d9a789c9d651
antoniovillena <espineter@yahoo.com>
2016-07-10 15:11:57 +0200

    Dejo el core SMS como estaba antes y borro archivos que no se usan

--- cores/MasterSystem/make.bat
+++ cores/MasterSystem/make.bat
@@ -3,7 +3,7 @@ SET speed=3
 SET ruta_ucf=src\sms
 SET ruta_bat=..\
 rem call %ruta_bat%genxst.bat
-rem call %ruta_bat%generar.bat v2 src\sms_bd.bmm all.mem
-rem call %ruta_bat%generar.bat v3 src\sms_bd.bmm all.mem
-call %ruta_bat%generar.bat v4 src\sms_bd.bmm all.mem
-rem call %ruta_bat%generar.bat Ap src\sms_bd.bmm all.mem
+rem call %ruta_bat%generar.bat v2
+rem call %ruta_bat%generar.bat v3
+call %ruta_bat%generar.bat v4
+rem call %ruta_bat%generar.bat Ap
