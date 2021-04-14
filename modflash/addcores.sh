cf9828d0aefdb1e838884a536a1211ee5581b7e0
Ivan Tatarinov <ivan-tat@ya.ru>
2021-04-14 17:08:50 +0300

    modflash: (fix) use `AddItem` tool from SDK

--- modflash/addcores.sh
+++ modflash/addcores.sh
@@ -5,7 +5,6 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-ADDITEM=./AddItem
 i=2
 
 Error() {
@@ -17,7 +16,7 @@ AddCore() {
 	local f=${3%.*}.tap
 	echo "Adding core $i: \"$2\" ($3)..."
 	GenRom $1 "$2" $3 $f || Error $?
-	$ADDITEM CORE$i $f || Error $?
+	AddItem CORE$i $f || Error $?
 	rm -f $f
 	let i+=1
 }
