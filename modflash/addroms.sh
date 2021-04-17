bb11a955348ce12750b25f7034d05bf5a88d89bc
Ivan Tatarinov <ivan-tat@ya.ru>
2021-04-17 23:01:32 +0300

    modflash: improved error handling when building, small fix in `Makefile`

--- modflash/addroms.sh
+++ modflash/addroms.sh
@@ -7,26 +7,28 @@
 
 i=0
 
-Error() {
-	echo "ERROR: Exit status $1. Stopped." >&2
-	exit $1
-}
-
 AddROM() {
 	local n=`stat --printf "%s" $3`
 	local i1=$((i+n/16384-1))
 	local f=${3%.*}.tap
 	echo "Adding ROM in slots $i-$i1: \"$2\" ($3)..."
-	GenRom $1 "$2" $3 $f || Error $?
-	AddItem ROM $i $f || Error $?
+	GenRom $1 "$2" $3 $f
+	AddItem ROM $i $f
 	rm -f $f
 	let i=i1+1
+}
+
+OnError() {
+	local err=$?
+	echo "ERROR: Exit status $err. Stopped." >&2
+	exit $err
 }
 
 OnExit() {
 	rm -f addroms.tmp
 }
 
+trap OnError ERR
 trap OnExit EXIT
 
 awk -F \; '/^[^#]+/{print "AddROM " $1 " " $2 " " gensub(/\\/, "/", "g", $3)}' roms.txt >addroms.tmp
