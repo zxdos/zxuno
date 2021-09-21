2824eda2aad3a5c7422ca4a3200c120556416306
Ivan Tatarinov <ivan-tat@ya.ru>
2021-09-21 18:31:09 +0300

    sdk/setenv.bat: command echoing mode preserved, internal `cmd.exe` commands are now uppercase, fixed platform's paths

--- sdk/setenv.bat
+++ sdk/setenv.bat
@@ -1,30 +1,42 @@
-@echo off
-rem SPDX-FileCopyrightText: 2021 Ivan Tatarinov <ivan-tat@ya.ru>
-rem
-rem SPDX-License-Identifier: GPL-3.0-or-later
+@REM SPDX-FileType: SOURCE
+@REM SPDX-FileCopyrightText: 2021 Ivan Tatarinov <ivan-tat@ya.ru>
+@REM SPDX-License-Identifier: GPL-3.0-or-later
 
-if not x%ZXSDK% == x exit /b
-set ZXSDK=%~dp0
-set ZXSDK=%ZXSDK:~0,-1%
-set _path=%ZXSDK%
-if %PROCESSOR_ARCHITECTURE% == X86 (
-  set ZXSDK_PLATFORM=%ZXSDK%\windows-x86
-) else if %PROCESSOR_ARCHITECTURE% == AMD64 (
-  set ZXSDK_PLATFORM=%ZXSDK%\windows-x86
-) else if %PROCESSOR_ARCHITECTURE% == EM64T (
-  set ZXSDK_PLATFORM=%ZXSDK%\windows-x86
-) else (
-  echo WARNING: Unsupported processor architecture "%PROCESSOR_ARCHITECTURE%"
-  set ZXSDK_PLATFORM=%ZXSDK%\unknown
+@IF NOT "%ZXSDK%" == "" EXIT /b
+
+@SET ZXSDK=%~dp0
+@SET ZXSDK=%ZXSDK:~0,-1%
+@SET _path=%ZXSDK%
+
+@REM *** Platform ***
+
+@IF "%PROCESSOR_ARCHITECTURE%" == "X86" (
+  SET ZXSDK_PLATFORM=%ZXSDK%\windows-x86
+) ELSE IF "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
+  SET ZXSDK_PLATFORM=%ZXSDK%\windows-x86
+) ELSE IF "%PROCESSOR_ARCHITECTURE%" == "EM64T" (
+  SET ZXSDK_PLATFORM=%ZXSDK%\windows-x86
+) ELSE (
+  ECHO WARNING: Unsupported processor architecture: %PROCESSOR_ARCHITECTURE%
+  SET ZXSDK_PLATFORM=%ZXSDK%\unknown
 )
-set _path=%_path%;%ZXSDK_PLATFORM%\bin
-set _path=%_path%;%ZXSDK_PLATFORM%\lib
-set SDCCHOME=%ZXSDK_PLATFORM%\opt\sdcc
-set _path=%_path%:%SDCCHOME%\bin
-set SDCCINCLUDE=%SDCCHOME%\include
-set SDCCLIB=%SDCCHOME%\lib
-set Z88DK=%ZXSDK%\src\z88dk
-set _path=%_path%;%Z88DK%\bin
-set ZCCCFG=%Z88DK%\lib\config
-set PATH=%_path%;%PATH%
-set _path=
+@SET _path=%_path%;%ZXSDK_PLATFORM%\bin
+@SET _path=%_path%;%ZXSDK_PLATFORM%\lib
+
+@REM *** SDCC ***
+
+@SET SDCCHOME=%ZXSDK_PLATFORM%\opt\sdcc
+@SET _path=%_path%;%SDCCHOME%\bin
+@SET SDCCINCLUDE=%SDCCHOME%\include
+@SET SDCCLIB=%SDCCHOME%\lib
+
+@REM *** z88dk ***
+
+@SET Z88DK=%ZXSDK_PLATFORM%\opt\z88dk
+@SET _path=%_path%;%Z88DK%\bin
+@SET ZCCCFG=%Z88DK%\lib\config
+
+@REM *** PATH variable ***
+
+@SET PATH=%_path%;%PATH%
+@SET _path=
