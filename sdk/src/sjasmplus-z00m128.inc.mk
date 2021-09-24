1f2c3f26accb818f6da12da10cfe179ef9bbd791
Ivan Tatarinov <ivan-tat@ya.ru>
2021-09-24 22:20:02 +0300

    sdk/src/sjasmplus-z00m128.inc.mk: added `current` version of SJAsmPlus by Aprisobal as an option

--- sdk/src/sjasmplus-z00m128.inc.mk
+++ sdk/src/sjasmplus-z00m128.inc.mk
@@ -15,39 +15,62 @@ ifeq ($(_DoBuild),1)
 
  ifeq ($(USE_SJASMPLUS_VERSION),1.18.2)
 
-SJASMPLUS_ARCHIVE		= $(DOWNLOADS)/sjasmplus-z00m128/v1.18.2.tar.gz
+SJASMPLUS_ARCHIVE		= v1.18.2.tar.gz
+SJASMPLUS_ARCHIVE_URL		= https://github.com/z00m128/sjasmplus/archive/refs/tags/$(SJASMPLUS_ARCHIVE)
 SJASMPLUS_ARCHIVE_SHA256	= 114807bf53d3526b4d1ae7d40f3050b9ee98220df74931efc1e6d1fe5aba3d02
 SJASMPLUS_ARCHIVE_TYPE		= .tar.gz
 SJASMPLUS_ARCHIVE_SUBDIR	= sjasmplus-1.18.2
 
-$(DOWNLOADS)/sjasmplus-z00m128/v1.18.2.tar.gz: | $(DOWNLOADS)/sjasmplus-z00m128
-	wget -c https://github.com/z00m128/sjasmplus/archive/refs/tags/$(@F) -O $@
-
  else ifeq ($(USE_SJASMPLUS_VERSION),1.18.3)
 
-SJASMPLUS_ARCHIVE		= $(DOWNLOADS)/sjasmplus-z00m128/v1.18.3.tar.gz
+SJASMPLUS_ARCHIVE		= v1.18.3.tar.gz
+SJASMPLUS_ARCHIVE_URL		= https://github.com/z00m128/sjasmplus/archive/refs/tags/$(SJASMPLUS_ARCHIVE)
 SJASMPLUS_ARCHIVE_SHA256	= ce0707070946e3439756c0577dd9c4b16a1a4b75cc8bbf1b2be9c6638957be85
 SJASMPLUS_ARCHIVE_TYPE		= .tar.gz
 SJASMPLUS_ARCHIVE_SUBDIR	= sjasmplus-1.18.3
 
-$(DOWNLOADS)/sjasmplus-z00m128/v1.18.3.tar.gz: | $(DOWNLOADS)/sjasmplus-z00m128
-	wget -c https://github.com/z00m128/sjasmplus/archive/refs/tags/$(@F) -O $@
+ else ifeq ($(USE_SJASMPLUS_VERSION),current)
+
+SJASMPLUS_ARCHIVE		= master.zip
+SJASMPLUS_ARCHIVE_URL		= https://github.com/z00m128/sjasmplus/archive/refs/heads/$(SJASMPLUS_ARCHIVE)
+SJASMPLUS_ARCHIVE_TYPE		= .zip
+SJASMPLUS_ARCHIVE_SUBDIR	= sjasmplus-master
 
  else
   $(error Unknown SJAsmPlus version selected: $(USE_SJASMPLUS_VERSION))
  endif
 
-build-sjasmplus: | sjasmplus-z00m128/.extracted sjasmplus-z00m128.mk
-	$(MAKE) -w -C sjasmplus -f ../sjasmplus-z00m128.mk prefix=$(shell realpath --relative-to=sjasmplus-z00m128 $(prefix))
+ifeq ($(USE_SJASMPLUS_VERSION),current)
 
-sjasmplus-z00m128/.extracted: $(SJASMPLUS_ARCHIVE)
-	rm -rf $(@D)
+$(DOWNLOADS)/sjasmplus-z00m128/$(SJASMPLUS_ARCHIVE): | $(DOWNLOADS)/sjasmplus-z00m128
+	wget $(SJASMPLUS_ARCHIVE_URL) -O $@
+
+sjasmplus-z00m128/.extracted: $(DOWNLOADS)/sjasmplus-z00m128/$(SJASMPLUS_ARCHIVE)
+	$(RM) -r $(@D)
+	extract.sh $<\
+	 --type $(SJASMPLUS_ARCHIVE_TYPE)\
+	 --subdir $(SJASMPLUS_ARCHIVE_SUBDIR)\
+	 --output $(@D)
+	touch $@
+
+else
+
+$(DOWNLOADS)/sjasmplus-z00m128/$(SJASMPLUS_ARCHIVE): | $(DOWNLOADS)/sjasmplus-z00m128
+	wget -c $(SJASMPLUS_ARCHIVE_URL) -O $@
+
+sjasmplus-z00m128/.extracted: $(DOWNLOADS)/sjasmplus-z00m128/$(SJASMPLUS_ARCHIVE)
+	$(RM) -r $(@D)
 	extract.sh $<\
 	 --sha256 $(SJASMPLUS_ARCHIVE_SHA256)\
 	 --type $(SJASMPLUS_ARCHIVE_TYPE)\
 	 --subdir $(SJASMPLUS_ARCHIVE_SUBDIR)\
 	 --output $(@D)
 	touch $@
+
+endif
+
+build-sjasmplus: | sjasmplus-z00m128/.extracted sjasmplus-z00m128.mk
+	$(MAKE) -w -C sjasmplus-z00m128 -f ../sjasmplus-z00m128.mk prefix=$(shell realpath --relative-to=sjasmplus-z00m128 $(prefix))
 
 install-sjasmplus: | sjasmplus-z00m128/.extracted sjasmplus-z00m128.mk
 	$(MAKE) -w -C sjasmplus-z00m128 -f ../sjasmplus-z00m128.mk prefix=$(shell realpath --relative-to=sjasmplus-z00m128 $(prefix)) install
@@ -58,7 +81,7 @@ uninstall-sjasmplus: | sjasmplus-z00m128.mk
 	if test -d sjasmplus-z00m128; then\
 		$(MAKE) -w -C sjasmplus-z00m128 -f ../sjasmplus-z00m128.mk prefix=$(shell realpath --relative-to=sjasmplus-z00m128 $(prefix)) uninstall;\
 	else\
-		rm -f $(DESTDIR)$(bindir)/sjasmplus$(EXESUFFIX);\
+		$(RM) $(DESTDIR)$(bindir)/sjasmplus$(EXESUFFIX);\
 	fi
 
 clean-sjasmplus: | sjasmplus-z00m128.mk
